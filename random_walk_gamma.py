cfac36d96b1aed08f5e825265da3ab29400d494e
Melody Hu <40373356+shinyumh@users.noreply.github.com>
2023-07-26 00:20:12 -0700

    Add files via upload

--- random_walk_gamma.py
+++ random_walk_gamma.py
@@ -2,8 +2,11 @@
 """random_walk_gamma.py"""
 
 from scipy.special import gamma
+from math import sqrt
+import matplotlib.pyplot as plt
 
-def main() -> None:
+def randomwalk(n):
+    """calculate and plots the expected final distance of a random walk of n steps on a unit lattice having dimension d"""
     dimen = []
 
     # evenly find 100 values from between 1 - 25
@@ -16,17 +19,19 @@ def main() -> None:
     # calculate the expected value of the final distance for every dimension
     # using the scipy function, scipy.special.gamma, and the equation given
     for d in dimen:
-        expected.append((2 / d)*((gamma((d + 1) / 2)) / gamma(d/2)) ** 2)
+        expected.append(sqrt(2 * n / d)*((gamma((d + 1) / 2)) / gamma(d/2)) ** 2)
+
+    plt.plot(dimen,expected)
+    plt.xlabel("dimensions")
+    plt.ylabel("expected final distance")
+    plt.show()
 
     # print out the result
-    for d, e in list(zip(dimen, expected)):
-        print(f"for distance {d}, the expected value is {e}")
+    # for d, e in list(zip(dimen, expected)):
+    #     print(f"for distance {d}, the expected value is {e}")
+
+def main() -> None:
+    randomwalk(20000)
 
 if __name__ == "__main__":
     main()
-
-
-
-#additional comments:
-
-'''incorrect output. you need to plot the values'''
